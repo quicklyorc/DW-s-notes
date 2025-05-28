da90fb78fd2acd9765168b47736685a6792b1a64
Lifepass <dwstarwow@gmail.com>
2025-05-28 21:32:16 +0900

    base_commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.header-navbar-icon').on('click', function () {
+    location.href = 'landing_page.html'
+});
+
+$('#header-navbar-btn>div>button').eq(0).on('click', function () {
+    location.href = 'landing_page.html'
+});
+
+$('#header-navbar-btn>div>button').eq(1).on('click', function () {
+    location.href = 'about_me.html'
+});
+
+$('#header-navbar-btn>div>button').eq(2).on('click', function () {
+    location.href = 'projects.html'
+});
+
+$('#header-navbar-btn>div>button').eq(3).on('click', function () {
+    location.href = 'study.html'
+});
+
+$('.header-navbar-caffebtn').on('click', function () {
+    location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+});
